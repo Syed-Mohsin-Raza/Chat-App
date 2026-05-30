596bdec2ac93a0f0e93aec531700ac2102f87397
Syed-Mohsin-Raza <raza.mohsin1441@gmail.com>
2026-05-30 11:26:05 +0500

    feat: create database schema for users module

---
+++ Backend/src/modules/users/user.model.js
@@ -0,0 +1,47 @@
+import { selectFields } from "express-validator/lib/field-selection";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 30,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false, 
+    },
+    avatar: {
+        key: { type: String, default: '' },
+        url: { type: String, default: '' },
+    },
+    bio: {
+        type: String,
+        default: '',
+        maxlength: 160,
+    },
+    isOnline: {
+        type: Boolean,
+        default: false,
+    },
+    lastSeen: {
+        type: Date,
+        default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
